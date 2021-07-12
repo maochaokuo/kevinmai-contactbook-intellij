52566c11540a3f77d092ff4afa94295816e1c420
Thomas Mao <tm1813@gmail.com>
2021-07-12 22:10:25 +0800

    can use reset! to run -main now

--- README.md
+++ README.md
@@ -1,5 +1,45 @@
 # kevinmai-contactbook-intellij
 
+## [1] Reitit Ring Set Up
+step 1
+```
+clj -A:run
+```
+will show hello world
+
+step 2
+- make default rest api, port 4000
+- again, clj -A:run, see Server Started
+  to confirm
+- use Insomia or postman, to test get
+  http://localhost:4000/api
+- will get "ok"
+
+step3
+- add atom server
+- add stop-server
+- in vscode, Ctrl-Alt-c Ctrl-Alt-j, choose deps.edn,
+  then no option, will enter REPL, wait until Jack-in
+  done
+
+## change log
+### 2021/7/11
+- make default rest api
+
+### 2021/7/10
+- initial commit
+- show Hello World
+
+#
+#
+#
+#
+#
+#
+#
+#
+# kevinmai-contactbook-intellij
+
 A Clojure library designed to ... well, that part is up to you.
 
 ## Usage
--- src/kevinmai_contactbook_intellij/core.clj
+++ src/kevinmai_contactbook_intellij/core.clj
@@ -1,6 +1,37 @@
-(ns kevinmai-contactbook-intellij.core)
+;(ns contacts.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(ns kevinmai-contactbook-intellij.core
+  (:require [org.httpkit.server :refer [run-server]]
+            [reitit.ring :as ring]))
+
+(defonce server (atom nil))
+
+(defn stop-server []
+  (when-not (nil? @server)
+    ((@server :timeout 100)
+     (reset! server nil)
+     (println "Server stopped"))))
+
+(def app
+  (ring/ring-handler
+    (ring/router
+      [["/api" {:get (fn [req]
+                       {:status 200
+                        :body   "ok"})}]])))
+
+(defn -main []
+  (println "Server started")
+  (reset! server (run-server app {:port 4004})))
+
+(defn restart-server []
+  (stop-server)
+  (-main))
+
+(comment
+  (-main)
+  )
+;
+;(defn foo
+;  "I don't do a whole lot."
+;  [x]
+;  (println x "Hello, World!"))
